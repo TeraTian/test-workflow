18fcfc6646273ac23a08cfb5d0805de9fbea7bd4
tera <frogjay@aliyun.com>
2024-08-20 17:51:10 +0800

    1

--- internal/types/container/keepalived_container_collector_host.go
+++ internal/types/container/keepalived_container_collector_host.go
@@ -6,17 +6,13 @@ import (
 	"path/filepath"
 	"regexp"
 	"strconv"
-	"strings"
 	"syscall"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/strslice"
 	"github.com/docker/docker/client"
 	"github.com/hashicorp/go-version"
-	"github.com/sirupsen/logrus"
-	
 	"github.com/mehdy/keepalived-exporter/internal/collector"
 	"github.com/mehdy/keepalived-exporter/internal/types/utils"
+	"github.com/sirupsen/logrus"
 )
 
 // KeepalivedContainerCollectorHost implements Collector for when Keepalived is on container and Keepalived Exporter is on a host.
